210ec4615aeab5decea04bf79a41e734bb7ead60
Fabian <fabiannavarro@web.de>
2019-07-15 22:10:29 +0200

    css changes

--- js/script.js
+++ js/script.js
@@ -3,24 +3,32 @@ function toggle_content(content){
     $("#terminetext").hide();
     $("#buechertext").hide();
     $("#videostext").hide();
+    $("#contacttext").hide();
     $("#vitatext").fadeToggle();
   }else if (content==="gigs") {
     $("#vitatext").hide();
     $("#videostext").hide();
     $("#buechertext").hide();
+    $("#contacttext").hide();
     $("#terminetext").fadeToggle();
   }else if (content==="videos") {
     $("#vitatext").hide();
     $("#terminetext").hide();
     $("#buechertext").hide();
+    $("#contacttext").hide();
     $("#videostext").fadeToggle();
   }else if (content==="books") {
     $("#vitatext").hide();
     $("#terminetext").hide();
     $("#videostext").hide();
+    $("#contacttext").hide();
     $("#buechertext").fadeToggle();
   }else if (content==="contact") {
-
+    $("#vitatext").hide();
+    $("#terminetext").hide();
+    $("#videostext").hide();
+    $("#buechertext").hide();
+    $("#contacttext").fadeToggle();
   }else if (content==="home") {
     clear();
   }
